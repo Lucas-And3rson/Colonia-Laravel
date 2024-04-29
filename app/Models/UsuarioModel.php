6817ef03c946e7aa4a9a98763e9a81361df79b64
Lucas Anderson <117605065+Lucas-And3rson@users.noreply.github.com>
2024-04-29 17:53:21 -0300

    Delete app/Models/UsuarioModel.php

--- app/Models/UsuarioModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Database\Eloquent\Model;
-
-class Usuario extends Model
-{
-    protected $fillable = ['nome', 'email', 'nivel', 'senha'];
-    protected $hidden = ['senha'];
-}
